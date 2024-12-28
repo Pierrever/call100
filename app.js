02c11d163ccb1570d4d564e7de46241cadc435f0
Petr Vurm <petr.vurm@gmail.com>
2024-12-28 12:11:04 +0100

    first EJS templates

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //const express = require("express");
-
+const path = require("path");
 const app = require("express")();
 const authRoutes = require("./routes/auth.routes");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(authRoutes);
 
