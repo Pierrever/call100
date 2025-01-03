d8ce27b5e25e10d4ae5c858041a7e05dd82235f5
Petr Vurm <petr.vurm@gmail.com>
2025-01-03 21:53:54 +0100

    styling, some bug in base.css

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-//const express = require("express");
+const express = require("express");
 const path = require("path");
-const app = require("express")();
+const app = express();
 const authRoutes = require("./routes/auth.routes");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
 
 app.use(authRoutes);
 
