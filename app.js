f91dec3cc21bb240fea5ed0ed96a0d39b7dcfba5
Petr Vurm <petr.vurm@gmail.com>
2025-01-10 20:28:25 +0100

    sessions added!

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const csurf = require("csurf");
 const path = require("path");
+const expressSession = require("express-session");
 const authRoutes = require("./routes/auth.routes");
 const db = require("./data/database");
 const addCsrfTokenMw = require("./middlewares/csrf-token");
+const errorHandleMw = require("./middlewares/error-handler");
+const createSessionConfig = require("./config/session");
 
 const app = express();
 
@@ -13,9 +16,13 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+const sessionConfig = createSessionConfig();
+app.use(expressSession(sessionConfig));
 app.use(csurf());
 app.use(addCsrfTokenMw);
 app.use(authRoutes);
+
+app.use(errorHandleMw);
 
 db.connectToDatabase()
   .then(() => app.listen(3000))
