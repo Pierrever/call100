20f063a15d582c49a4f5017d389b325701c5ddb1
Petr Vurm <petr.vurm@gmail.com>
2025-01-10 18:16:26 +0100

    csrf

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const db = require("./data/database");
+const csurf = require("csurf");
 const path = require("path");
-const app = express();
 const authRoutes = require("./routes/auth.routes");
+const db = require("./data/database");
+const addCsrfTokenMw = require("./middlewares/csrf-token");
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -10,6 +13,8 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(csurf());
+app.use(addCsrfTokenMw);
 app.use(authRoutes);
 
 db.connectToDatabase()
